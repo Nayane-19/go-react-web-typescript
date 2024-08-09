bff6c4356ad6aa4c47583153b075292b8707ab5c
Nayane <nayanemenezes2011@gmail.com>
2024-08-08 21:40:54 -0300

    feature: configure errorBoundary and queryClient

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,31 @@
-import { Toaster } from 'sonner'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Room } from './pages/room/room'
-import { CreateRoom } from './pages/create-room/create-room'
+import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Room } from "./pages/room/room";
+import { CreateRoom } from "./pages/create-room/create-room";
+import { ErrorPage } from "./pages/error-page/error-page";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <CreateRoom />
+    path: "/",
+    element: <CreateRoom />,
+    ErrorBoundary: () => <ErrorPage />,
   },
   {
-    path: '/room/:roomId',
-    element: <Room />
-  }
-])
+    path: "/room/:roomId",
+    element: <Room />,
+    ErrorBoundary: () => <ErrorPage />,
+  },
+]);
 
 export const App = () => {
   return (
-    <>
-    <Toaster invert richColors />
-    <RouterProvider router={router} />
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <Toaster invert richColors />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+};
